c78b75513752efa5453debf2e9f124112a719096
Ivan <vanyashakhovv@gmail.com>
2022-11-13 19:48:12 +0600

    +

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $('.block_packet').hover(
   (e) => {
-    console.log(e.target.className);
     e.target.className === 'block_packet' &&
       $(e.target).css({
         '-webkit-box-shadow': '0px 0px 41px 9px rgba(0, 0, 0, 0.2)',
@@ -12,8 +11,8 @@ $('.block_packet').hover(
   (e) => {
     e.target.className === 'block_packet' &&
       $(e.target).css({
-        '-webkit-box-shadow': '0px ',
-        '-moz-box-shadow': '0px ',
+        '-webkit-box-shadow': 'none ',
+        '-moz-box-shadow': 'none ',
         'box-shadow': 'none',
         border: '1px solid #BFBFBF',
       });
