ac650e6b9047b089421a825e580cb8aba8ed016f
Ivan <vanyashakhovv@gmail.com>
2022-11-14 10:59:04 +0600

    +

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # [Live Demo](https://thatv1n.github.io/TaskGlamurnenko/).
-
-start 12.11.22 12:00
-end 12.11.22 16:00
-= 4 hours
-
-start 13.11.22 17:00
-end 13.11.22 19:30
-= 2.30 hours
--- js/main.js
+++ js/main.js
@@ -1,20 +1,56 @@
-$('.block_packet').hover(
-  (e) => {
-    e.target.className === 'block_packet' &&
-      $(e.target).css({
-        '-webkit-box-shadow': '0px 0px 41px 9px rgba(0, 0, 0, 0.2)',
-        '-moz-box-shadow': '0px 0px 41px 9px rgba(0, 0, 0, 0.2)',
-        'box-shadow': '0px 0px 41px 9px rgba(0, 0, 0, 0.2)',
-        border: '2px solid #BFBFBF ',
-      });
-  },
-  (e) => {
-    e.target.className === 'block_packet' &&
-      $(e.target).css({
-        '-webkit-box-shadow': 'none ',
-        '-moz-box-shadow': 'none ',
-        'box-shadow': 'none',
-        border: '1px solid #BFBFBF',
-      });
-  },
-);
+$(document).ready(function () {
+  $(function () {
+    $('.block_packet').hover(
+      (e) => {
+        e.target.className === 'block_packet' &&
+          $(e.target).css({
+            '-webkit-box-shadow': '0px 0px 41px 9px rgba(0, 0, 0, 0.2)',
+            '-moz-box-shadow': '0px 0px 41px 9px rgba(0, 0, 0, 0.2)',
+            'box-shadow': '0px 0px 41px 9px rgba(0, 0, 0, 0.2)',
+            border: '2px solid #BFBFBF ',
+          });
+      },
+      (e) => {
+        e.target.className === 'block_packet' &&
+          $(e.target).css({
+            '-webkit-box-shadow': 'none ',
+            '-moz-box-shadow': 'none ',
+            'box-shadow': 'none',
+            border: '1px solid #BFBFBF',
+          });
+      },
+    );
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 600) {
+        $('header').css({
+          position: 'fixed',
+          background: 'black',
+          top: '0',
+          width: '100%',
+          left: '0',
+          padding: '5px 40px',
+        });
+      } else {
+        $('header').css({
+          position: 'absolute',
+          background: 'none',
+          top: '',
+          width: '',
+          left: '',
+          padding: '0',
+        });
+      }
+    });
+
+    $('#click-top').click(function () {
+      $('body,html').animate(
+        {
+          scrollTop: 0,
+        },
+        800,
+      );
+      return false;
+    });
+  });
+});
